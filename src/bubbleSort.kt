ba7689eea417e37e84db0a3e02288c7ea771af3f
MaximZazulin <88067751+MaximZazulin@users.noreply.github.com>
2022-09-02 04:07:53 +0500

    Step1

--- src/bubbleSort.kt
+++ src/bubbleSort.kt
@@ -1,7 +1,7 @@
-//  Author      : Maxim Zazulin
+/* Author      : Maxim Zazulin */
 fun main () {
     var list: Array<Int> = arrayOf(1, 3, 2, 5, 1, 8, 4, 2, 7, 4)
-    var otvet: Array<Int> = arrayOf(1, 1, 2, 2, 3, 4, 4, 5, 7, 8)
+    val otvet: Array<Int> = arrayOf(1, 1, 2, 2, 3, 4, 4, 5, 7, 8)
     println("Initial data")
     list.forEach { name -> print(name) }
     println("\nResult")
@@ -14,14 +14,13 @@ fun main () {
 }
 fun bubbleSort(array: Array<Int>): Array<Int> {
     var buffer : Int
-    for (schetchik in array.size - 2 downTo 0) {
-        for (i in 0..schetchik) {
+    for (schetchik in array.size - 2 downTo 0)
+        for (i in 0..schetchik)
             if (array[i] > array[i + 1]) {
                 buffer = array[i]
                 array[i] = array[i + 1]
                 array[i + 1] = buffer
             }
-        }
-    }
     return array
 }
+
